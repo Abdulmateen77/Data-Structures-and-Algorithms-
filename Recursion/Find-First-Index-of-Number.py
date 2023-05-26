39d956ef3acd8f2536bdd1fadf9ca04fe61253f1
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-05-26 22:12:11 +0100

    Update Find-First-Index-of-Number.py

--- Recursion/Find-First-Index-of-Number.py
+++ Recursion/Find-First-Index-of-Number.py
@@ -1,18 +1,21 @@
-def firstIndex(arr, x, index = 0):
-    # Please add your code here
-    
+def firstIndex(arr, x, index=0):
+    # Base case: if the index reaches the end of the array, return -1
     if index == len(arr):
         return -1
+    # Check if the current element is equal to x
     elif arr[index] == x:
+        # If found, return the current index
         return index
     else:
+        # Recursive case: search for x in the remaining part of the array
         return firstIndex(arr, x, index + 1)
-    
+
 
 # Main
 from sys import setrecursionlimit
 setrecursionlimit(11000)
-n=int(input())
-arr=list(int(i) for i in input().strip().split(' '))
-x=int(input())
-print(firstIndex(arr, x,index = 0))
+
+n = int(input())  # Input the number of elements in the array
+arr = list(int(i) for i in input().strip().split(' '))  # Input the array elements
+x = int(input())  # Input the value to search for
+print(firstIndex(arr, x, index=0))  # Print the index of the first occurrence of x
