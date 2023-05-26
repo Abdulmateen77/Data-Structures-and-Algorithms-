7d4f57b4ee4ce182466b8ef0b479b02587585be7
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-05-26 22:06:35 +0100

    Update SumOfArrayRecursively.py

--- Recursion/SumOfArrayRecursively.py
+++ Recursion/SumOfArrayRecursively.py
@@ -1,15 +1,18 @@
-def sumArray(arr,length):
-    # Please add your code here
+def sumArray(arr, length):
+    # Base case: if the length is less than or equal to 0, return 0
     if length <= 0:
         return 0
+    
+    # Recursive case: sum the elements from index 0 to length-1 and add it to the sum of remaining elements
     else:
-        return sumArray(arr,length-1) + arr[length-1]
+        return sumArray(arr, length-1) + arr[length-1]
 
 
 # Main
 from sys import setrecursionlimit
 setrecursionlimit(11000)
-n=int(input())
-arr=list(int(i) for i in input().strip().split(' '))
-length = len(arr)
-print(sumArray(arr,length))
+
+n = int(input())  # Input the number of elements in the array
+arr = list(int(i) for i in input().strip().split(' '))  # Input the array elements
+length = len(arr)  # Calculate the length of the array
+print(sumArray(arr, length))  # Print the sum of the array elements
