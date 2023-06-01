81285dc8242908495abe249303c491268de13e89
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-06-01 15:18:26 +0100

    Update Geometric-Sum.py

--- Recursion/Geometric-Sum.py
+++ Recursion/Geometric-Sum.py
@@ -1,12 +1,20 @@
 def GeometricSum(k):
+    # Base case: when k reaches 0, return 1
     if k == 0:
         return 1
     else:
-        return 1/(2**k) + GeometricSum(k-1)
+        # Recursive case: calculate the current term as 1/(2^k)
+        # and add it to the sum of the previous terms
+        return 1 / (2 ** k) + GeometricSum(k - 1)
 
 def main():
+    # Read the number of terms to sum from user input
     k = int(input())
+
+    # Calculate the geometric sum using the GeometricSum function
     result = GeometricSum(k)
+
+    # Print the result with 5 decimal places
     print("{:.5f}".format(result))
 
 if __name__ == "__main__":
