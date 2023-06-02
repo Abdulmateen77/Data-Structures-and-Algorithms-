0c8866666f30d61647b54dc5c205a5f2c566568c
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-06-02 12:00:51 +0100

    Update TowerofHanoi.py

--- Recursion/TowerofHanoi.py
+++ Recursion/TowerofHanoi.py
@@ -1,16 +1,23 @@
 def towerofhanoi(n, source, aux, dest):
-    # Please add your code here
-    if n== 0:
+    # Base case: If n is 0, there are no disks to move, so return
+    if n == 0:
         return
+    
+    # Base case: If n is 1, move the disk from source to destination
     elif n == 1:
-        print(source+ " "+dest)
+        print(source + " " + dest)
         return
     
-    towerofhanoi(n-1, source,dest,aux)
-    print(source +" "+dest)
-    towerofhanoi(n-1,aux,source,dest)
+    # Recursive case:
+    # Move (n-1) disks from source to auxiliary tower, using the destination tower as the auxiliary tower
+    towerofhanoi(n - 1, source, dest, aux)
     
+    # Move the nth disk from source to destination
+    print(source + " " + dest)
+    
+    # Move (n-1) disks from auxiliary tower to destination tower, using the source tower as the auxiliary tower
+    towerofhanoi(n - 1, aux, source, dest)
 
 
-n=int(input())
-towerofhanoi(n, 'a', 'b', 'c')
+n = int(input())  # Read the number of disks
+towerofhanoi(n, 'a', 'b', 'c')  # Call the towerofhanoi function with the initial setup
