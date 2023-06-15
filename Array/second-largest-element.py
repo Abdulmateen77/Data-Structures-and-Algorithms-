7ddec0a0d9e73049f5ea73bfac03636bb17e8203
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-06-15 11:07:48 +0100

    Update second-largest-element.py

--- Array/second-largest-element.py
+++ Array/second-largest-element.py
@@ -1,5 +1,20 @@
 class Solution:
     def print2largest(self, arr, n):
+        first = -1  # Initialize the first largest element as -1
+        second = -1  # Initialize the second largest element as -1
+
+        for i in range(len(arr)):
+            if arr[i] > first:
+                second = first  # Update the second largest element to the previous first largest element
+                first = arr[i]  # Update the first largest element
+            elif arr[i] > second and arr[i] != first:
+                second = arr[i]  # Update the second largest element
+
+        return second  # Return the second largest element
+
+#better approach
+class Solution:
+    def print2largest(self, arr, n):
         arr.sort()  # Sort the array in ascending order
         max_num = arr[-1]  # Get the maximum number from the array
 
