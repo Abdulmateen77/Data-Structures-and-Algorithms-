559202a15c6454ee221dfff45f3d2fabff94cf73
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-06-22 12:00:11 +0100

    Update Find-Duplicate.py

--- Array/Find-Duplicate.py
+++ Array/Find-Duplicate.py
@@ -1,3 +1,4 @@
+#brute force approach
 def duplicateNumber(arr, n):
     # Iterate through each element in the array
     for i in range(n):
@@ -12,12 +13,14 @@ def duplicateNumber(arr, n):
 
 #optimize approach
 def findDuplicate(arr, n) :
-    #Your code goes here
-    seen = set()
-    for i in range(n):
+    seen = set() #initialise a set
+    # Iterate through each element in the array
+    for i in range(n): 
+        #check wheather the element exists in the array
         if arr[i] in seen:
+            #return the duplicate
             return arr[i]
-        seen.add(arr[i])
+        seen.add(arr[i]) # Add the unique element to the set
 
 # Main
 n = int(input())  # Number of elements in the array
