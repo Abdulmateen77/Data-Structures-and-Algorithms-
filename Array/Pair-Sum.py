105031d209193dcd201ba657e5afe208b95bea3b
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-06-22 13:55:18 +0100

    Update Pair-Sum.py

--- Array/Pair-Sum.py
+++ Array/Pair-Sum.py
@@ -1,6 +1,6 @@
 from sys import stdin
 
-
+#Brute Force Approach
 def pairSum(arr, n, x):
     count = 0  # Initialize a variable to count the number of pairs
     for i in range(n-1):
@@ -9,3 +9,23 @@ def pairSum(arr, n, x):
             if arr[i] + arr[j] == x:
                 count += 1  # Increment the count if a pair is found
     return count  # Return the count of pairs
+
+#Optimal Approach
+
+def pairSum(arr, n, num):
+# Function to count the number of pairs with a given sum in an array
+# arr: input array
+# n: length of the array
+# num: target sum
+
+num_dict = {}  # Dictionary to store the frequency of array elements
+count = 0  # Variable to store the count of pairs
+
+for i in range(n):
+    complement = num - arr[i]  # Calculate the complement of the current element
+    if complement in num_dict:
+        # If the complement is present in the dictionary, increment the count by its frequency
+        count += num_dict[complement]
+    num_dict[arr[i]] = num_dict.get(arr[i], 0) + 1  # Update the frequency of the current element
+
+return count
