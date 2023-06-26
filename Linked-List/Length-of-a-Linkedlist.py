74c32dd50fc0482019269e6fdadbce1b7fdd22b7
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-06-26 15:09:21 +0100

    Create Length-of-a-Linkedlist.py

---
+++ Linked-List/Length-of-a-Linkedlist.py
@@ -0,0 +1,21 @@
+class Node:
+def init(self, data):
+self.data = data
+self.next = None
+
+Function to calculate the length of a linked list
+head: the head node of the linked list
+def length(head):
+"""
+This function iterates through the linked list, counting the number of nodes,
+and returns the length of the linked list.
+"""
+temp = head  # Start with the head node
+lengthLL = 0
+
+# Traverse the linked list until the end is reached (temp is None)
+while temp is not None:
+    lengthLL += 1  # Increment the length counter
+    temp = temp.next  # Move to the next node
+
+return lengthLL
