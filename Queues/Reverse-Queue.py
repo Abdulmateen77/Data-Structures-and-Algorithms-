b8cdc2a5cacc744f6d2a2f0d8775f725ca0a9445
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-07-13 10:11:29 +0100

    Create Reverse-Queue.py

---
+++ Queues/Reverse-Queue.py
@@ -0,0 +1,53 @@
+from sys import stdin, setrecursionlimit
+import queue
+
+setrecursionlimit(10 ** 6)
+
+def reverseQueue(inputQueue):
+    # Check if the size of the input queue is less than or equal to 1, no need to reverse
+    if inputQueue.qsize() <= 1:
+        return inputQueue
+    
+    # Get the front element of the queue
+    Element = inputQueue.get()
+
+    # Reverse the remaining elements of the queue using recursion
+    reverseQueue(inputQueue)
+
+    # Put the front element at the end of the reversed queue
+    inputQueue.put(Element)
+
+
+
+
+
+
+'''-------------- Utility Functions --------------'''
+
+def takeInput():
+    # Read the number of elements in the queue
+    n = int(stdin.readline().strip())
+
+    # Create a queue and populate it with the given values
+    qu = queue.Queue()
+    values = list(map(int, stdin.readline().strip().split()))
+
+    for i in range(n):
+        qu.put(values[i])
+
+    return qu
+
+# Main
+t = int(stdin.readline().strip())
+
+while t > 0:
+    qu = takeInput()
+    reverseQueue(qu)
+    
+    # Print the reversed queue
+    while not qu.empty():
+        print(qu.get(), end=" ")
+        
+    print()
+    
+    t -= 1
