3dd3b49b79f311af31408d3f248766f0e848bda1
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-07-15 15:51:39 +0100

    Create Preorder-Binary-Tree.py

---
+++ Trees/Binary-tree/Preorder-Binary-Tree.py
@@ -0,0 +1,73 @@
+from sys import stdin, setrecursionlimit
+import queue
+
+setrecursionlimit(10 ** 6)
+
+# Following the structure used for Binary Tree
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def preOrder(root):
+    # Function to perform pre-order traversal of a binary tree
+    # Your code goes here
+    if root is None:
+        return
+    
+    # Print the data of the current node
+    print(root.data, end=" ")
+
+    # Recursively traverse the left subtree
+    preOrder(root.left)
+
+    # Recursively traverse the right subtree
+    preOrder(root.right)
+
+
+# Taking level-order input using fast I/O method
+def takeInput():
+    levelOrder = list(map(int, stdin.readline().strip().split(" ")))
+    start = 0
+
+    length = len(levelOrder)
+
+    # Create the root node of the binary tree
+    root = BinaryTreeNode(levelOrder[start])
+    start += 1
+
+    q = queue.Queue()
+    q.put(root)
+
+    # Construct the binary tree using level-order traversal
+    while not q.empty():
+        currentNode = q.get()
+
+        leftChild = levelOrder[start]
+        start += 1
+
+        # Create the left child node and link it to the current node
+        if leftChild != -1:
+            leftNode = BinaryTreeNode(leftChild)
+            currentNode.left = leftNode
+            q.put(leftNode)
+
+        rightChild = levelOrder[start]
+        start += 1
+
+        # Create the right child node and link it to the current node
+        if rightChild != -1:
+            rightNode = BinaryTreeNode(rightChild)
+            currentNode.right = rightNode
+            q.put(rightNode)
+
+    return root
+
+
+# Main
+root = takeInput()
+
+# Perform pre-order traversal of the binary tree and print the nodes
+preOrder(root)
