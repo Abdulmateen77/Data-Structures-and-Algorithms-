1fe500e516f5f10d60e0f1dae4c4ba04d9673280
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-07-20 11:13:17 +0100

    Update Diameter-Of-Binary-Tree.py

--- Trees/Binary-tree/Diameter-Of-Binary-Tree.py
+++ Trees/Binary-tree/Diameter-Of-Binary-Tree.py
@@ -1,43 +1,44 @@
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# Importing necessary modules and setting recursion limit to handle large binary trees
+from sys import stdin, setrecursionlimit
+import queue
+setrecursionlimit(10 ** 6)
 
+# Following is the structure used to represent the Binary Tree Node
+class BinaryTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+# Function to calculate the height of a binary tree starting from the given root
+def height(root):
+    if root == None:
+        return 0
+
+    # Calculate the height recursively as 1 plus the maximum of the heights of the left and right subtrees
+    return 1 + max(height(root.left), height(root.right))
+
+
+# Function to find the diameter of a binary tree
 def diameterOfBinaryTree(root):
-    def height(node):
-        if not node:
-            return 0
-        return 1 + max(height(node.left), height(node.right))
+    # Base case: If the root is None, the diameter of the tree is 0
+    if root == None:
+        return 0
+    
+    # Calculate the height of the left and right subtrees
+    lh = height(root.left)
+    rh = height(root.right)
 
-    def diameter_helper(node):
-        if not node:
-            return 0
+    # Calculate the maximum diameter that passes through the current root
+    h = lh + rh
 
-        # Get the height of the left and right subtrees
-        lh = height(node.left)
-        rh = height(node.right)
+    # Recursively find the diameters of the left and right subtrees
+    leftroot = diameterOfBinaryTree(root.left)
+    rightroot = diameterOfBinaryTree(root.right)
 
-        # Calculate the diameter passing through the current node
-        current_diameter = lh + rh
-
-        # Recursively find the diameter in the left and right subtrees
-        left_diameter = diameter_helper(node.left)
-        right_diameter = diameter_helper(node.right)
-
-        # Return the maximum of the current diameter and the diameters of the left and right subtrees
-        return max(current_diameter, left_diameter, right_diameter)
-
-    return diameter_helper(root)
-
-# Example usage:
-# Build the binary tree manually
-root = TreeNode(1)
-root.left = TreeNode(2)
-root.right = TreeNode(3)
-root.left.left = TreeNode(4)
-root.left.right = TreeNode(5)
-
-# Calculate and print the diameter of the binary tree
-diameter = diameterOfBinaryTree(root)
-print("Diameter of the binary tree:", diameter)
+    # Return the maximum of the following three values:
+    # 1. The maximum diameter that goes through the current root (h).
+    # 2. The diameter of the left subtree (leftroot).
+    # 3. The diameter of the right subtree (rightroot).
+    # Adding 1 to the maximum value to account for the current root node in the diameter calculation.
+    return 1 + max(h, leftroot, rightroot)
