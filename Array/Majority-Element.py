078e73951935fcde367ec3332ec0936875c86355
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-07-28 22:36:38 +0100

    Update Majority-Element.py

--- Array/Majority-Element.py
+++ Array/Majority-Element.py
@@ -19,3 +19,25 @@ class Solution:
 
         # Return the majority element found in the list
         return MajorityEle
+
+def main():
+    # Test Case 1
+    nums1 = [3, 2, 3]
+    sol1 = Solution()
+    result1 = sol1.majorityElement(nums1)
+    print("Majority Element in nums1:", result1)  # Output: 3
+
+    # Test Case 2
+    nums2 = [2, 2, 1, 1, 1, 2, 2]
+    sol2 = Solution()
+    result2 = sol2.majorityElement(nums2)
+    print("Majority Element in nums2:", result2)  # Output: 2
+
+    # Test Case 3
+    nums3 = [1, 2, 2, 3, 2, 4, 2, 5, 2]
+    sol3 = Solution()
+    result3 = sol3.majorityElement(nums3)
+    print("Majority Element in nums3:", result3)  # Output: 2
+
+if __name__ == "__main__":
+    main()
