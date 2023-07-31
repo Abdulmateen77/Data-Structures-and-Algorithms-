22036630796e6293582c643b3690f330eb407bbe
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-07-31 15:53:22 +0100

    Update Print-Generic-tree.py

--- Trees/Generic-Tree/Print-Generic-tree.py
+++ Trees/Generic-Tree/Print-Generic-tree.py
@@ -21,3 +21,28 @@ def printTree(root):
     # Recursively call the printTree function for each child node.
     for child in root.children:
         printTree(child)
+
+
+# Main function to build the tree and test the printTree function
+if __name__ == "__main__":
+    # Create the root node
+    root = TreeNode(1)
+
+    # Create child nodes and add them to the root
+    child1 = TreeNode(2)
+    child2 = TreeNode(3)
+    child3 = TreeNode(4)
+
+    root.children.append(child1)
+    root.children.append(child2)
+    root.children.append(child3)
+
+    # Create grandchildren nodes and add them to child1
+    grandchild1 = TreeNode(5)
+    grandchild2 = TreeNode(6)
+
+    child1.children.append(grandchild1)
+    child1.children.append(grandchild2)
+
+    # Print the tree
+    printTree(root)
