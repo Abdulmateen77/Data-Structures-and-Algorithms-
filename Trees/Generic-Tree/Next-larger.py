2565fb059396a837732d55ed73edf16ec17e077e
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-02 17:38:53 +0100

    Update Next-larger.py

--- Trees/Generic-Tree/Next-larger.py
+++ Trees/Generic-Tree/Next-larger.py
@@ -1,32 +1,36 @@
-from sys import stdin,setrecursionlimit
+from sys import stdin, setrecursionlimit
+# Increase recursion limit to handle large trees
 setrecursionlimit(10**6)
+
+# Definition of TreeNode class
 class treeNode:
     def __init__(self, data):
         self.data = data
         self.children = []
 
+# Function to find the next largest node in the tree
 def nextLargest(tree, n):
-    if tree == None:
+    if tree is None:
         return None
     
     ans_node = None
     temp = None
 
+    # If current node's data is greater than n, it can potentially be the next largest
     if tree.data > n:
         ans_node = tree
     
+    # Explore the children nodes
     for child in tree.children:
         temp = nextLargest(child, n)
 
+        # If temp is not None and satisfies the conditions, update ans_node
         if temp is not None and (ans_node is None or (temp.data < ans_node.data and temp.data > n)):
             ans_node = temp
     
     return ans_node
 
-
-
-
-
+# Function to create a level-wise tree from an array
 def createLevelWiseTree(arr):
     root = treeNode(int(arr[0]))
     q = [root]
@@ -42,10 +46,4 @@ def createLevelWiseTree(arr):
             q.append(temp)
         i += childCount
     return root
-# Main
-arr = list(int(x) for x in stdin.readline().strip().split())
-n = int(input())
-tree = createLevelWiseTree(arr)
-result_node = nextLargest(tree, n)
-if result_node:
-    print(result_node.data)
+
