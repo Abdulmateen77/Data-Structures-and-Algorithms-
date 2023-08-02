3f1ef5c2ed03e3b4181fd71b75337b9592b7dc48
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-02 13:28:01 +0100

    Update Count-leaf-Nodes.py

--- Trees/Generic-Tree/Count-leaf-Nodes.py
+++ Trees/Generic-Tree/Count-leaf-Nodes.py
@@ -1,4 +1,6 @@
 import sys
+
+# Define the treeNode class
 class treeNode:
     def __init__(self, data):
         self.data = data
@@ -6,26 +8,30 @@ class treeNode:
     def __str__(self):
         return str(self.data)
 
+# Function to count the number of leaf nodes in the tree
 def leafNodeCount(tree):
+    # Base case: if the node has no children, it is a leaf node
     if tree.children == []:
         return 1
     leaf = 0
     
+    # Count leaf nodes in each child
     for child in tree.children:
         leaf = leaf + leafNodeCount(child)
         
     return leaf
 
+# Function to create a level-wise generic tree
 def createLevelWiseTree(arr):
     root = treeNode(int(arr[0]))
     q = [root]
     size = len(arr)
     i = 1
-    while i<size:
+    while i < size:
         parent = q.pop(0)
         childCount = int(arr[i])
         i += 1
-        for j in range(0,childCount):
+        for j in range(childCount):
             temp = treeNode(int(arr[i+j]))
             parent.children.append(temp)
             q.append(temp)
@@ -34,6 +40,10 @@ def createLevelWiseTree(arr):
 
 # Main
 sys.setrecursionlimit(10**6)
+
+# Read input and create the generic tree
 arr = list(int(x) for x in input().strip().split(' '))
 tree = createLevelWiseTree(arr)
+
+# Print the count of leaf nodes
 print(leafNodeCount(tree))
