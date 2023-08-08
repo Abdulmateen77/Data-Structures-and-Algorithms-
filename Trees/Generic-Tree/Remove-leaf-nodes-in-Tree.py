3054db1b80ddd62b80633774b88be8e11fd262c0
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-08 14:41:59 +0100

    Update Remove-leaf-nodes-in-Tree.py

--- Trees/Generic-Tree/Remove-leaf-nodes-in-Tree.py
+++ Trees/Generic-Tree/Remove-leaf-nodes-in-Tree.py
@@ -1,6 +1,5 @@
-## Read input as specified in the question.
-## Print output as specified in the question.
 import queue 
+
 class treeNode:
     def __init__(self, data):
         self.data = data
@@ -8,6 +7,7 @@ class treeNode:
     def __str__(self):
         return str(self.data)
 
+# Function to remove leaf nodes from the tree
 def removeLeafNodes(root):
     if root is None:
         return None
@@ -15,40 +15,43 @@ def removeLeafNodes(root):
     q = queue.Queue()
     q.put(root)
     
+    # Perform level-order traversal
     while not q.empty():
         current = q.get()
         
-        # Remove leaf nodes by filtering out children
+        # Remove leaf nodes by filtering out children that have no children
         current.children = [child for child in current.children if len(child.children) > 0]
         
+        # Enqueue non-leaf children for further processing
         for child in current.children:
             q.put(child)
     
     return root
 
-
+# Function to create a level-wise tree from given input
 def createLevelWiseTree(arr):
     root = treeNode(int(arr[0]))
     q = [root]
     size = len(arr)
     i = 1
-    while i<size:
+    while i < size:
         parent = q.pop(0)
         childCount = int(arr[i])
         i += 1
-        for j in range(0,childCount):
+        for j in range(0, childCount):
             temp = treeNode(int(arr[i+j]))
             parent.children.append(temp)
             q.append(temp)
         i += childCount
     return root
 
+# Function to print a level-wise tree
 def printLevelWiseTree(root):
     q = queue.Queue()
     q.put(root)
     q.put(None)
     
-    while q.empty() is False:
+    while not q.empty():
         front = q.get()
         if front is None:
             if q.empty():
@@ -57,15 +60,16 @@ def printLevelWiseTree(root):
                 print()
                 q.put(None)
         else:
-            print(front.data,end= ' ')
+            print(front.data, end=' ')
             for child in front.children:
                 q.put(child)
             
-        
-    
-    
 # Main
 arr = list(int(x) for x in input().strip().split(' '))
 root = createLevelWiseTree(arr)
+
+# Remove leaf nodes from the tree
 removeLeafNodes(root)
+
+# Print the modified tree level-wise
 printLevelWiseTree(root)
