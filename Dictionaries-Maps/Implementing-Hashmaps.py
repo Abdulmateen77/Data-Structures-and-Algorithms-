e31225c9df0e182ba8456040d0402835498ed5f0
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-12 10:15:49 +0100

    Update Implementing-Hashmaps.py

--- Dictionaries-Maps/Implementing-Hashmaps.py
+++ Dictionaries-Maps/Implementing-Hashmaps.py
@@ -18,4 +18,16 @@ class mao:
 
   def insert(self,key,value):
     hc = hash(key)
+    index = self.getBucketIndex(hc)
+    head = self.bucket[index]
+    while head id not None:
+      if head.key == key:
+        head.value = value
+        return
+      head = head.next
+    head = self.bucket[index]
+    newNode = mapNode(key,value)
+    newNode.next = head
+    self.bucket[index] = newNode
+    self.count += 1
     
