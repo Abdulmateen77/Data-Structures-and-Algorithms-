6f8b96467ff37ce7d15783b5245c3a357dede6f2
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-18 12:11:51 +0100

    Update Max-Priority-Queue.py

--- Priority-Queues/Max-Priority-Queue.py
+++ Priority-Queues/Max-Priority-Queue.py
@@ -1,44 +1,48 @@
+# Define a class to represent a node in the priority queue with an element and a priority
 class PriorityQueueNode:
     def __init__(self, ele, priority):
         self.ele = ele
         self.priority = priority
 
+# Define a class for a Priority Queue
 class PriorityQueue:
     def __init__(self):
-        self.pq = []
+        self.pq = []  # Initialize an empty list to store priority queue elements
 
+    # Check if the priority queue is empty
     def isEmpty(self):
-        #Implement the isEmpty() function here
         return self.getSize() == 0
     
+    # Get the size of the priority queue
     def getSize(self):
-        #Implement the getSize() function here
         return len(self.pq)
 
+    # Get the maximum priority (highest value) element in the priority queue
     def getMax(self):
-        #Implement the getMax() function here
         if self.isEmpty():
             return None
         return self.pq[0].priority
     
+    # Move a newly inserted element up the priority queue to maintain the max heap property
     def __procolateUp(self):
-        childIndex = self.getSize()-1
+        childIndex = self.getSize() - 1
 
         while childIndex > 0:
             parentIndex = (childIndex - 1) // 2
+
             if self.pq[childIndex].priority > self.pq[parentIndex].priority:
                 self.pq[childIndex], self.pq[parentIndex] = self.pq[parentIndex], self.pq[childIndex]
                 childIndex = parentIndex
-
             else:
                 break
-
-    def insert(self,ele,priority):
-        #Implement the insert() function here
-        pqNode = PriorityQueueNode(ele, priority)
-        self.pq.append(pqNode)
-        self.__procolateUp()
     
+    # Insert an element with a given priority into the priority queue
+    def insert(self, ele, priority):
+        pqNode = PriorityQueueNode(ele, priority)
+        self.pq.append(pqNode)  # Add the new element to the end of the list
+        self.__procolateUp()    # Move the element up to maintain the max heap property
+    
+    # Move an element down the priority queue to maintain the max heap property
     def __procolateDown(self):
         parentIndex = 0
         leftChildIndex = 2 * parentIndex + 1
@@ -63,43 +67,41 @@ class PriorityQueue:
             leftChildIndex = 2 * parentIndex + 1
             rightChildIndex = 2 * parentIndex + 2 
 
-
-
-
+    # Remove and return the maximum priority element from the priority queue
     def removeMax(self):
-        #Implement the removeMax() function here
         if self.isEmpty():
             return None
-        ele = self.pq[0].priority
-        self.pq[0] = self.pq[self.getSize()-1]
+        ele = self.pq[0].priority  # Store the priority of the maximum priority node
+        self.pq[0] = self.pq[self.getSize() - 1]
         self.pq.pop()
-        self.__procolateDown()
-
+        self.__procolateDown()  # Move the new root element down to maintain the max heap property
         return ele
 
-
+# Create an instance of the PriorityQueue class
 myPq = PriorityQueue()
+
+# Read user input and perform priority queue operations based on the input
 curr_input = [int(ele) for ele in input().split()]
 choice = curr_input[0]
-i=1
+i = 1
 while choice != -1:
     if choice == 1:
         element = curr_input[i]
-        i+=1
-        myPq.insert(element,element)
+        i += 1
+        myPq.insert(element, element)  # Insert the element into the priority queue
     elif choice == 2:
-        print(myPq.getMax())
+        print(myPq.getMax())  # Print the maximum priority element in the priority queue
     elif choice == 3:
-        print(myPq.removeMax())
+        print(myPq.removeMax())  # Remove and print the maximum priority element
     elif choice == 4:
-        print(myPq.getSize())
+        print(myPq.getSize())  # Print the size of the priority queue
     elif choice == 5:
         if myPq.isEmpty():
-            print('true')
+            print('true')  # Print "true" if the priority queue is empty
         else:
-            print('false')
-        break
+            print('false')  # Print "false" if the priority queue is not empty
+        break  # Exit the loop
     else:
-        pass
+        pass  # Do nothing for invalid choices
     choice = curr_input[i]
-    i+=1
+    i += 1
