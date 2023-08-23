090000fe18ec5dba20f36d52afd2f6f10a6bc6d3
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-23 16:11:16 +0100

    Create Buy-the-ticket.py

---
+++ Priority-Queues/Buy-the-ticket.py
@@ -0,0 +1,104 @@
+from sys import stdin
+import sys
+import heapq as heap
+
+class LinkedListNode :
+    def __init__(self, data) :
+        self.data = data
+        self.next = None
+        
+class Queue :
+    def __init__(self) :
+        self.head = None 
+        self.tail = None
+        self.size = 0
+        
+    def enqueue(self, data) :
+        newNode = LinkedListNode(data)
+        if self.head is None :
+            self.head = self.tail = newNode
+        else :
+            self.tail.next = newNode
+            self.tail = newNode
+        self.size += 1
+        return
+        
+    def dequeue(self) :
+        if self.head is None :
+            return None
+        data = self.head.data
+        self.head = self.head.next
+        self.size -= 1
+        return data
+    
+    def getSize(self) :
+        return self.size
+
+    def isEmpty(self) :
+        if self.head is None :
+            return True
+        return False
+    
+    def peek(self) :
+        if self.head is None :
+            return None
+        return self.head.data
+    
+def buyTicket(arr, n, k) :
+    
+    priorityQueue = []
+    
+    for i in range(n):
+        heap.heappush(priorityQueue, -arr[i])
+
+    q = Queue()
+
+    for i in range(n):
+        q.enqueue(arr[i])
+    
+    time = 0
+
+    while not q.isEmpty():
+        if q.peek() == -priorityQueue[0]:
+            time += 1
+            q.dequeue()
+            heap.heappop(priorityQueue)
+            if k == 0:
+                return time
+            
+            else:
+                k -= 1
+        else:
+            q.enqueue(q.dequeue())
+            if k == 0:
+                k = q.getSize() -1
+            else:
+                k -= 1
+    
+    return time
+
+
+
+
+
+
+
+
+
+
+
+
+
+#taking input using fast I/O
+def takeInput() :
+    n = int(stdin.readline().strip())
+    if n == 0 :
+        return n, list(), int(stdin.readline().strip())
+    arr = list(map(int, stdin.readline().strip().split(" ")))
+    k = int(stdin.readline().strip())
+    return n, arr, k
+
+#main
+sys.setrecursionlimit(10**6)
+n, arr, k = takeInput()
+print(buyTicket(arr, n, k))
