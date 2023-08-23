d370107174b19d231a9ef262bb46c8299929a077
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-23 14:09:29 +0100

    Update Check-Max-Heap.py

--- Priority-Queues/Check-Max-Heap.py
+++ Priority-Queues/Check-Max-Heap.py
@@ -4,14 +4,16 @@ def checkMaxHeap(lst):
 
     # Iterate through the list up to the second-to-last element
     for i in range(n - 1):
-        LeftIndex = 2*i + 1
-        rightIndex = leftIndex + 1
-        # Check if the current element is greater than the next element
-        if leftIndex < n and  lst[leftIndex] > lst[i]:
-            return False
-
+        leftIndex = 2 * i + 1  # Calculate the index of the left child
+        rightIndex = leftIndex + 1  # Calculate the index of the right child
+        
+        # Check if the left child index is within the bounds of the list
+        if leftIndex < n and lst[leftIndex] > lst[i]:
+            return False  # If the left child is greater, it violates max-heap property
+        
+        # Check if the right child index is within the bounds of the list
         if rightIndex < n and lst[rightIndex] > lst[i]:
-            return False
+            return False  # If the right child is greater, it violates max-heap property
             
     return True  # If all conditions are met, the list is a max-heap
 
