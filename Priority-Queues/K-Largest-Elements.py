a82440517a23ecc553a4bdb12f601ba6205102fb
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-23 13:49:47 +0100

    Update K-Largest-Elements.py

--- Priority-Queues/K-Largest-Elements.py
+++ Priority-Queues/K-Largest-Elements.py
@@ -1,16 +1,19 @@
-import heapq
-def kLargest(lst, k):
-    heap = lst[0:k]
-    heapq.heapify(heap)
-    n = len(lst)
+import heapq  # Importing the heapq module for min-heap operations
 
-    for i in range(k,n):
-        if heap[0] < lst[i]:
-            heapq.heapreplace(heap, lst[i])
-    return heap
+def kLargest(lst, k):
+    heap = lst[0:k]  # Create a heap of the first k elements from the list
+    heapq.heapify(heap)  # Convert the list into a min-heap
+    n = len(lst)  # Total number of elements in the list
+    
+    # Iterate over the remaining elements in the list
+    for i in range(k, n):
+        if heap[0] < lst[i]:  # If the smallest element in the heap is smaller than the current element
+            heapq.heapreplace(heap, lst[i])  # Replace the smallest element with the current element
+    return heap  # Return the k largest elements in a min-heap
+
 # Main Code
-n=int(input())
-lst=list(int(i) for i in input().strip().split(' '))
-k=int(input())
-ans=kLargest(lst, k)
-print(*ans, sep='\n')
+n = int(input())  # Input the total number of elements in the list
+lst = list(int(i) for i in input().strip().split(' '))  # Input the list elements
+k = int(input())  # Input the value of k for finding k largest elements
+ans = kLargest(lst, k)  # Call the kLargest function to get the k largest elements
+print(*ans, sep='\n')  # Print the k largest elements separated by newlines
