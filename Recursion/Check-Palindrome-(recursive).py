e27fd245987fc13cc3cbe3bfe5479fd1e1755a9e
Abdulmateen77 <69627857+Abdulmateen77@users.noreply.github.com>
2023-08-27 23:59:33 +0100

    Update Check-Palindrome-(recursive).py

--- Recursion/Check-Palindrome-(recursive).py
+++ Recursion/Check-Palindrome-(recursive).py
@@ -1,3 +1,13 @@
+# importing the sys module
+import sys
+ 
+# the setrecursionlimit function is
+# used to modify the default recursion
+# limit set by python. Using this,
+# we can increase the recursion limit
+# to satisfy our needs
+ 
+sys.setrecursionlimit(10**6)
 import recursion
 
 class Solution:
