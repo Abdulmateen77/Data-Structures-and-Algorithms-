8c242393ab2b709af3e658dfea1d57ceed20c077
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-05 21:04:20 +0100

    Update Minimum-length-word.py

--- strings/Minimum-length-word.py
+++ strings/Minimum-length-word.py
@@ -1,20 +1,35 @@
-input_str = input()  # Avoid using "str" as a variable name since it shadows the built-in str type
+# Read a string from user input
+input_str = input()
+
+# Initialize variables to keep track of the minimum length and corresponding string
 min_len = len(input_str)
 min_str = input_str
 
+# Initialize a temporary string to store the current word
 curr_str = ""
 
+# Loop through each character in the input string
 for char in input_str:
+    # Check if the character is a space
     if char == " ":
-        if len(curr_str) < min_len:  # Correct the variable name and the comparison condition
-            min_len = len(curr_str)  # Correct the variable name
+        # If the length of the current word is shorter than the minimum length seen so far
+        if len(curr_str) < min_len:
+            # Update the minimum length
+            min_len = len(curr_str)
+            # Update the corresponding string
             min_str = curr_str
+        # Reset the current word
         curr_str = ""
     else:
+        # If the character is not a space, append it to the current word
         curr_str += char
 
-if len(curr_str) < min_len:  # Correct the variable name and the comparison condition
-    min_len = len(curr_str)  # Correct the variable name
+# Check the length of the last word in case it's the shortest
+if len(curr_str) < min_len:
+    # Update the minimum length
+    min_len = len(curr_str)
+    # Update the corresponding string
     min_str = curr_str
 
+# Print the shortest word
 print(min_str)
