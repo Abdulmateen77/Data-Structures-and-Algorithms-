ac6bf5799806af55a2147d66a0efc3a3f3517951
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-08 16:10:22 +0100

    Update Frog-jump.py

--- Dynamic-Programming/Frog-jump.py
+++ Dynamic-Programming/Frog-jump.py
@@ -1,28 +1,45 @@
-from os import *
-from sys import *
-from collections import *
-from math import *
+# Import specific modules and types you need.
+from typing import List
 
-from typing import *
-
+# Global array to store dynamic programming results.
 dp = []
 
-def f(n: int, heights: List[int], N:int) -> int:
-    global dp 
+# Recursive function to find the minimum cost to reach the end.
+def f(n: int, heights: List[int], N: int) -> int:
+    global dp
+
+    # Base case: If we have reached the end, return 0.
     if n == N:
         return 0
-    
-    elif n == N-1:
-        return abs(heights[N-1] - heights[N-2])
+
+    # Base case: If we are one step away from the end, return the absolute height difference.
+    elif n == N - 1:
+        return abs(heights[N - 1] - heights[N - 2])
+
+    # If the result for the current step 'n' is already computed, return it.
     if dp[n] != -1:
         return dp[n]
+
+    # Recursive cases:
     else:
-        dp[n] =  min(f(n+1,heights,N) + abs(heights[n-1]-heights[n]), f(n+2,heights,N) +abs(heights[n-1] - heights[n+1]))
+        # Calculate the cost of jumping one step and add it to the result of the next step.
+        jump_one_step = f(n + 1, heights, N) + abs(heights[n - 1] - heights[n])
+
+        # Calculate the cost of jumping two steps and add it to the result of the step after that.
+        jump_two_steps = f(n + 2, heights, N) + abs(heights[n - 1] - heights[n + 1])
+
+        # Choose the minimum cost between jumping one step or two steps.
+        dp[n] = min(jump_one_step, jump_two_steps)
+
+        # Return the minimum cost for reaching the end from this step.
         return dp[n]
-         
+
+# Main function to calculate the minimum cost for the frog to reach the end.
 def frogJump(n: int, heights: List[int]) -> int:
     global dp
-    # Write your code here.
-    dp = [-1] * (n+1)
-    
+
+    # Initialize the dynamic programming array with -1 values.
+    dp = [-1] * (n + 1)
+
+    # Start the recursion from the first step (index 1) and return the result.
     return f(1, heights, n)
