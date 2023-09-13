d1a7a764d110c810350593afe2afc22a371d093e
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-13 20:14:16 +0100

    Update Min-Steps-to-one-using-DP.py

--- Dynamic-Programming/Min-Steps-to-one-using-DP.py
+++ Dynamic-Programming/Min-Steps-to-one-using-DP.py
@@ -1,25 +1,36 @@
+# Import necessary module for reading input
 from sys import stdin
+
+# Import a constant representing the maximum integer value
 from sys import maxsize as MAX_VALUE
 
+# Function to calculate the minimum steps to reduce 'n' to 1
+def countMinStepsToOne(n):
+    # Create a list to store the minimum steps for each number from 1 to n
+    dp = [0] * (n + 1)
 
-def countMinStepsToOne(n) :
+    # Iterate through the numbers from 2 to n
+    for i in range(2, n + 1):
+        # Initialize the minimum steps with 1 plus the minimum steps for (i-1)
+        miniSteps = 1 + dp[i - 1]
 
-    dp = [0] * (n+1)
-
-    for i in range(2, n+1):
-        miniSteps = 1 + dp[i -1]
-
+        # Check if (i-1) is divisible by 2 and update the minimum steps accordingly
         if i % 2 == 0:
-           miniSteps = min(miniSteps, 1 + dp[i // 2])
+            miniSteps = min(miniSteps, 1 + dp[i // 2])
 
-        
+        # Check if (i-1) is divisible by 3 and update the minimum steps accordingly
         if i % 3 == 0:
             miniSteps = min(miniSteps, 1 + dp[i // 3])
-        
+
+        # Store the minimum steps in the dp list for the current number 'i'
         dp[i] = miniSteps
-    
+
+    # The minimum steps to reduce 'n' to 1 is stored in dp[n]
     return dp[n]
 
-#main
-n = int(stdin.readline().rstrip())
-print(countMinStepsToOne(n))
+# Main function
+n = int(stdin.readline().rstrip())  # Read the input 'n'
+result = countMinStepsToOne(n)      # Call the function to calculate minimum steps
+print(result)                        # Print the result
+
+# Note: The code reads 'n' from input, calculates the minimum steps, and prints the result.
