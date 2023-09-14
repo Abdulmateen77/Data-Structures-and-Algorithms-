4e68704fb34e3814ce1998eb48541426c65c8612
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-14 14:30:22 +0100

    Update Rod-cutting-problem.py

--- Dynamic-Programming/Rod-cutting-problem.py
+++ Dynamic-Programming/Rod-cutting-problem.py
@@ -1,46 +1,44 @@
+# Import necessary modules
 from sys import stdin
 import sys
 
-
+# Function to calculate the maximum cost by cutting a rod of length 'n'
 def cutRod(price, n):
-
-    # Write your code here
+    # Initialize a list to store the maximum cost for each rod length
     cost = [0 for i in range(n+1)]
 
+    # The cost for a rod of length 0 is 0
     cost[0] = 0
 
+    # Iterate through each possible rod length from 1 to 'n'
     for i in range(1, n+1):
-        maxCost = -sys.maxsize-1
+        maxCost = -sys.maxsize-1  # Initialize the maximum cost as a very small value
 
+        # Try all possible ways of cutting the rod and find the maximum cost
         for j in range(i):
             maxCost = max(maxCost, price[j] + cost[i-j-1])
 
+        # Store the maximum cost for the current rod length 'i'
         cost[i] = maxCost
 
-    return cost[n] 
+    # The maximum cost for a rod of length 'n' is stored in cost[n]
+    return cost[n]
 
-
-
-
-
-
-
-
-
-
-
-# Taking input using fast I/O.
+# Function to take input using fast I/O
 def takeInput():
-    n = int(input())
+    n = int(input())  # Read the number of test cases
 
+    # Read the price list for each test case
     price = list(map(int, input().strip().split(" ")))
 
     return price, n
 
-
-# Main.
-t = int(input())
+# Main function
+t = int(input())  # Read the number of test cases
 while t:
-    price, n = takeInput()
-    print(cutRod(price, n))
-    t = t-1
+    price, n = takeInput()  # Read input for each test case
+    print(cutRod(price, n))  # Calculate and print the maximum cost
+    t = t - 1  # Decrement the number of test cases
+
+# Note: The code reads the number of test cases 't', and for each test case, it reads the price list and rod length,
+# calculates the maximum cost using the cutRod function, and prints the result.
