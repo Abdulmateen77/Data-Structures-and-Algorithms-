7976506401fdbe74d3a03eb20cccfdc6c8ec4bab
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-19 21:59:11 +0100

    Update Check-Array-Rotation.py

--- Array/Check-Array-Rotation.py
+++ Array/Check-Array-Rotation.py
@@ -1,1 +1,11 @@
-#Function-Check-Array-Rotation
+# Function to check for rotation in an array
+def arrayRotateCheck(arr, n):
+    # Iterate through the array elements up to the second-to-last element
+    for i in range(0, n - 1):
+        # Check if the current element is greater than the next element
+        if arr[i] > arr[i + 1]:
+            # If true, it indicates a rotation, return the index of the next element
+            return i + 1
+    
+    # If no rotation is found, return 0 (no rotation)
+    return 0
