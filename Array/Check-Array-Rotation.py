fb4d30fe6e1e2396d743d00bb33c4038ece066c4
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-19 21:59:53 +0100

    Update Check-Array-Rotation.py

--- Array/Check-Array-Rotation.py
+++ Array/Check-Array-Rotation.py
@@ -9,3 +9,28 @@ def arrayRotateCheck(arr, n):
     
     # If no rotation is found, return 0 (no rotation)
     return 0
+
+# Test case 1: Array with no rotation
+arr1 = [1, 2, 3, 4, 5]
+n1 = len(arr1)
+print(arrayRotateCheck(arr1, n1))  # Expected output: 0 (No rotation)
+
+# Test case 2: Array with rotation at the beginning
+arr2 = [4, 5, 1, 2, 3]
+n2 = len(arr2)
+print(arrayRotateCheck(arr2, n2))  # Expected output: 2 (Rotation at index 2)
+
+# Test case 3: Array with rotation in the middle
+arr3 = [3, 4, 5, 1, 2]
+n3 = len(arr3)
+print(arrayRotateCheck(arr3, n3))  # Expected output: 3 (Rotation at index 3)
+
+# Test case 4: Array with rotation at the end
+arr4 = [2, 3, 4, 5, 1]
+n4 = len(arr4)
+print(arrayRotateCheck(arr4, n4))  # Expected output: 4 (Rotation at index 4)
+
+# Test case 5: Array with a single element
+arr5 = [42]
+n5 = len(arr5)
+print(arrayRotateCheck(arr5, n5))  # Expected output: 0 (No rotation)
