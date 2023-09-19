0efeb07ea8ef1b03c56156c4196bc13f2f918675
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-19 21:58:58 +0100

    Update Check-Array-Rotation.py

--- Array/Check-Array-Rotation.py
+++ Array/Check-Array-Rotation.py
@@ -1,6 +1,1 @@
-def arrayRotateCheck(arr, n):
-    #Your code goes here
-    for i in range(0,n-1):
-        if arr[i] > arr[i+1]:
-            return i +1
-    return 0
+#Function-Check-Array-Rotation
