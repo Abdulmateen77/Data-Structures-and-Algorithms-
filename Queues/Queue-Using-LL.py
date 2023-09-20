b7d1b0f55a6dcb72ea1836756c6b909f08c7be87
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-20 23:53:03 +0100

    Update Queue-Using-LL.py

--- Queues/Queue-Using-LL.py
+++ Queues/Queue-Using-LL.py
@@ -56,35 +56,53 @@ class Queue:
         return self.__head.data
 
 # This is the main function of the program. It takes input from the user and performs the corresponding operations on the queue.
+
+# Define a main function to handle input and execute queue operations
 def main():
+    # Read the number of queries (q) from standard input
     q = int(stdin.readline().strip())
 
+    # Create an empty queue using a custom Queue class (not shown in this code snippet)
     queue = Queue()
 
+    # Loop through the queries
     while q > 0:
+        # Read and split the input line into a list of strings
         inputs = stdin.readline().strip().split(" ")
+        
+        # Convert the first element of the input list to an integer (choice)
         choice = int(inputs[0])
 
+        # Perform operations based on the choice value
         if choice == 1:
+            # Enqueue (add) an integer data element to the queue
             data = int(inputs[1])
             queue.enqueue(data)
 
         elif choice == 2:
+            # Dequeue (remove) and print the front element of the queue
             print(queue.dequeue())
 
         elif choice == 3:
+            # Print the front element of the queue without removing it
             print(queue.front())
 
         elif choice == 4:
+            # Print the size (number of elements) of the queue
             print(queue.getSize())
 
         else:
+            # Check if the queue is empty and print the result (true/false)
             if queue.isEmpty():
                 print("true")
             else:
                 print("false")
 
+        # Decrement the query counter
         q -= 1
 
+# Check if the script is being run as the main program
 if __name__ == "__main__":
+    # Call the main function to start executing the program
     main()
+
