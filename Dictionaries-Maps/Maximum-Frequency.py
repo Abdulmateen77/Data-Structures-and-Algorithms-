91c68f3c2cbd88241b8378958924112903dffaec
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-22 23:28:29 +0100

    Update Maximum-Frequency.py

--- Dictionaries-Maps/Maximum-Frequency.py
+++ Dictionaries-Maps/Maximum-Frequency.py
@@ -1,12 +1,16 @@
+# Import the sys module to use stdin for input
 from sys import stdin
-# Function to find the number with maximum frequency in an array
+
+# Function to find the number with the maximum frequency in an array
 def maximumFreq(arr):
+    # Create an empty dictionary to store number frequencies
     dict = {}
     
     # Count the frequency of each number using a dictionary
     for num in arr:
         dict[num] = dict.get(num, 0) + 1
     
+    # Initialize variables to track the maximum frequency and corresponding number
     maximum = -1
     maxNum = None
     
@@ -15,14 +19,18 @@ def maximumFreq(arr):
         if dict[num] > maximum:
             maximum = dict[num]
             maxNum = num
-    #returns maximum frequency number in an array
+    
+    # Return the number with the maximum frequency
     return maxNum
 
 # Read the input for the array size
 ArrayInput = int(input())
 
-# Read the input array elements
+# Read the input array elements and convert them to integers
 arr = list(int(i) for i in input().strip().split(' ')[:ArrayInput])
 
-# Call the maximumFreq function and print the result
-print(maximumFreq(arr))
+# Call the maximumFreq function to find the number with the maximum frequency
+result = maximumFreq(arr)
+
+# Print the result (number with maximum frequency) to the console
+print(result)
