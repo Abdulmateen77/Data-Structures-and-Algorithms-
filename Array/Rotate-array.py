09f1735cce23cf23472e30c6301d3de73a67b2ee
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-09-30 23:53:19 +0100

    Update Rotate-array.py

--- Array/Rotate-array.py
+++ Array/Rotate-array.py
@@ -1,15 +1,22 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
-        Do not return anything, modify nums in-place instead.
+        Rotate the elements of the given 'nums' list to the right by 'k' positions.
+        
+        Parameters:
+        - nums (List[int]): The input list of integers to be rotated.
+        - k (int): The number of positions to rotate the list to the right.
+        
+        Returns:
+        - None: The function modifies 'nums' in-place and does not return a new list.
         """
         n = len(nums)
         
-        # If k is greater than the array length, reduce it to the equivalent rotation within the array length
+        # If 'k' is greater than the length of the array, reduce it to the equivalent rotation within the array length
         if n < k:
             k = k % n
         
         # Rotate the array in-place by concatenating the subarrays
         nums[:] = nums[n - k:] + nums[:n - k]
         
-        # No need to return nums since the modification is done in-place
+        # No need to return 'nums' since the modification is done in-place
