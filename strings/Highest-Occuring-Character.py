611bb3a1068f3a3523e3eeea49495c4184279e06
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-10-16 23:54:41 +0100

    Update Highest-Occuring-Character.py

--- strings/Highest-Occuring-Character.py
+++ strings/Highest-Occuring-Character.py
@@ -2,17 +2,17 @@ from sys import stdin
 
 def highestOccuringChar(string):
     n = len(string)
-    frequency = [0] * 256  # Initialize an array to store the frequency of each character
+    frequency = [0] * 256  #Initialize an array to store the frequency of each character
     maxFrequency = 0
 
-    # Count the frequency of each character in the string
+    #Count the frequency of each character in the string
     for i in range(n):
         frequency[ord(string[i])] += 1  # Increment the frequency count for the character
         maxFrequency = max(maxFrequency, frequency[ord(string[i])])  # Update the max frequency
 
     answer = '0'
     
-    # Find the character with the highest frequency
+    #Find the character with the highest frequency
     for i in range(n):
         if frequency[ord(string[i])] == maxFrequency:
             answer = string[i]  # Update the answer with the current character
@@ -20,7 +20,7 @@ def highestOccuringChar(string):
 
     return answer
 
-# Main
+#Main
 string = stdin.readline().strip()  # Read the input string from standard input
 
 ans = highestOccuringChar(string)  # Call the highestOccuringChar function
