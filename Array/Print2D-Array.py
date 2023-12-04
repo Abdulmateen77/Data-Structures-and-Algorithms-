9b34ac926e7abe0b8a5ce558938ab3187b631969
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2023-12-04 23:56:49 +0000

    Update Print2D-Array.py

--- Array/Print2D-Array.py
+++ Array/Print2D-Array.py
@@ -1,15 +1,19 @@
+# Function to print a 2D array
 def print2DArray(mat, rows, cols):
+    # Iterate through each row of the matrix
     for i in range(rows):
         n = rows - i
+        # Print each element in the row
         while n != 0:
             for j in range(cols):
-                print(mat[i][j], end=' ')  # Print each element in the row
+                print(mat[i][j], end=' ')
             n -= 1  # Decrement the counter for the row
             print()  # Move to the next line after printing a row
 
 # Main
-mat = []
+mat = []  # Initialize an empty list to store the matrix
 
+# Read the dimensions of the matrix from user input
 li = input().strip().split()
 rows = int(li[0])  # Number of rows in the matrix
 cols = int(li[1])  # Number of columns in the matrix
