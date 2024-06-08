ad6aa4c7d4f752eaea7f28cf390c0ebf5610cf8f
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2024-06-08 18:30:23 +0100

    Update Delete-every-N-nodes.py

--- Linked-List/Delete-every-N-nodes.py
+++ Linked-List/Delete-every-N-nodes.py
@@ -5,7 +5,8 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-        
+
+
 def skipMdeleteN(head, M, N):
     # If the linked list is empty or has only one node, return the head
     if head is None or head.next is None:
