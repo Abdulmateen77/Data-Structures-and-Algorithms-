5044e367533979d6c91a81320485eba04aab3d59
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2024-08-30 15:59:45 +0100

    Update Reverse-the-First-K-Elements-in-the-Queue.py

--- Queues/Reverse-the-First-K-Elements-in-the-Queue.py
+++ Queues/Reverse-the-First-K-Elements-in-the-Queue.py
@@ -1,6 +1,7 @@
 from sys import stdin
 import queue
 
+
 def reverseKElements(inputQueue, k):
     #Check if k is 0 or greater than queue size, return the input queue as it is
     if k == 0 or k > inputQueue.qsize():
